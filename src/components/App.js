2e68d22efcbee99302503e34023fcca35e0672fc
Jonathan Cheng <jcheng461@gmail.com>
2022-10-26 23:42:56 +0900

    set up foundation for React app with App.js and starting components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/TicketControl.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TicketControl extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h3>Something</h3>
+      </React.Fragment>
+    );
+  }
+}
+
+export default TicketControl;
