d213c7afffe9d63a2ea062b719570d4702fd7baf
Jonathan Cheng <jcheng461@gmail.com>
2022-10-27 23:37:16 +0900

    WIP: set up list of tickets to be displayed

---
+++ src/components/TicketList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TicketList() {
+  return (
+    <React.Fragment>
+      <h3>List of Tickets</h3>
+    </React.Fragment>
+  );
+}
+
+export default TicketList();
