a867fd3c457fc05c15fa2597724ff32e68c70869
Jonathan Cheng <jcheng461@gmail.com>
2022-10-29 23:45:42 +0900

    set up mock ticketList. TicketList component properly uses map to display all Tickets in list to UI

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -2,11 +2,25 @@ import React from "react";
 import Ticket from "./Ticket";
 
 function TicketList() {
+  const ticketList = [
+    {
+      names: "Homer and Bart",
+      location: "Springfield Elementary",
+      issue: "Need a ride.",
+    },
+    {
+      names: "Lisa",
+      location: "Library",
+      issue: "Lost library card",
+    },
+  ];
   return (
     <React.Fragment>
       <h2>List of Tickets</h2>
       <hr />
-      <Ticket names="Homer and Bart" location="Springfield Elementary" issue="Need a ride." />
+      {ticketList.map((ticket) => (
+        <Ticket names={ticket.names} location={ticket.location} issue={ticket.issue} />
+      ))}
     </React.Fragment>
   );
 }
