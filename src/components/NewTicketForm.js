a5b8cb18a0d4e11205e140bc3e6f2a5dc000aee5
Jonathan Cheng <jcheng461@gmail.com>
2022-10-30 23:20:46 +0900

    set up TicketControl class component to handle state changes. WIP: set up additional components

---
+++ src/components/NewTicketForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewTicketForm() {
+  return (
+    <React.Fragment>
+      <h3>New Ticket Form</h3>
+    </React.Fragment>
+  );
+}
+
+export default NewTicketForm;
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,10 +1,36 @@
 import React from "react";
+import TicketList from "./TicketList";
+import NewTicketForm from "./NewTicketForm";
 
 class TicketControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formShowing: false,
+      mainTicketList: [],
+    };
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => ({ formShowing: !prevState.formShowing }));
+  };
+
   render() {
+    let currentlyDisplaying = null;
+    let buttonText = null;
+
+    if (this.state.formShowing) {
+      currentlyDisplaying = <NewTicketForm />;
+      buttonText = "Return to Ticket List";
+    } else {
+      currentlyDisplaying = <TicketList />;
+      buttonText = "Create New Ticket";
+    }
+
     return (
       <React.Fragment>
-        <h3>Something</h3>
+        {currentlyDisplaying}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
